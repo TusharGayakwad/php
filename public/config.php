ecc774f5d60014c9fb6c88ed143dce5a0c66020d
Tushar Gayakwad <109648133+TusharGayakwad@users.noreply.github.com>
2024-08-30 10:08:48 +0530

    Add files via upload

--- public/config.php
+++ public/config.php
@@ -1,14 +1,16 @@
 <?php
-define('DB_SERVER', 'database-2.c3qw4u6g6zrr.ap-northeast-1.rds.amazonaws.com');
-define('DB_USERNAME', 'user1');
-define('DB_PASSWORD', 'mypassword');
+define('DB_SERVER', 'database-1.chgcawess2vp.us-west-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'passwd');
 define('DB_NAME', 'mydb');
- 
-// Attempt to connect to mysql database 
+
+// Attempt to connect to MySQL database 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect to database. " . mysqli_connect_error());
 }
+
+echo "Connected successfully!";
 ?>
